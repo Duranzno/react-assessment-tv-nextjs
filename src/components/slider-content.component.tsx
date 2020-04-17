0b50377aaaaa3e7d92dfae1171a8d71fc893803d
Alejandro <aledurax@gmail.com>
2020-04-16 20:19:58 -0400

    ✅ Add Pager Test

--- src/components/slider-content.component.tsx
+++ src/components/slider-content.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Image from 'material-ui-image';
 import { grey } from '@material-ui/core/colors';
 import { TVSchedule } from '../models';
 
